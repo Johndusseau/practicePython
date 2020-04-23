d3f19a7aceb8573fabbf4ec6dea89dca89791211
john dusseau <johndusseau92@gmail.com>
2020-04-23 14:13:03 -0500

    update

--- practice.py
+++ practice.py
@@ -1,8 +1,26 @@
 import math
-mpg = 20
-cost_gallon = 3.1599
+'''milesPerGallon = 20 #float(input()) 
+dollarsPerGallon = 3.1599 #float(input())
 
-distance = mpg *cost_gallon
+miles_20 = (20 / milesPerGallon) * dollarsPerGallon
+miles_75 = (75 / milesPerGallon) * dollarsPerGallon
+miles_500 = (500 / milesPerGallon) * dollarsPerGallon
 
-print(distance)
-#print('{:.2f} {:.2f} {:.2f}'.format(your_value1, your_value2, your_value3))
+
+print(str(miles_20) + " " + str(miles_75) + " " str(miles_500))'''
+
+import math
+
+
+age = input()
+weight = input()
+hr = input()
+time = input()
+
+womens_cal = ((age * 0.074) - (weight * 0.05741) +
+              (hr * 0.4472) - 20.4022) * (time / 4.184)
+mens_cal = ((age * 0.2017) + (weight * 0.09036) +
+            (hr * 0.6309) - 55.0969) * (time / 4.184)
+
+print(womens_cal)
+print(mens_cal)
